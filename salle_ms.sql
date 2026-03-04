889e461df9aa88eedb308755e346d9de4c9f8ef7
Gabriel-L-F <gabriel.lefebvre_fristot@edu.devinci.fr>
2026-03-04 11:07:21 +0100

    ajout export bdd

---
+++ salle_ms.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 6.0.0-dev+20251125.142c0cf3be
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 04, 2026 at 10:04 AM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `salle_ms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercice`
+--
+
+CREATE TABLE `exercice` (
+  `id` int NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `muscle` set('pectoraux','dos','epaules','biceps','triceps','avant_bras','quadriceps','ischio_jambiers','fessiers','mollets','abdominaux') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `machine` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `exercice`
+--
+
+INSERT INTO `exercice` (`id`, `nom`, `muscle`, `machine`) VALUES
+(1, 'Développé couché', 'pectoraux,triceps', 1),
+(2, 'Tractions pronation', 'dos,biceps', 0),
+(3, 'Rowing barre', 'dos,biceps', 1),
+(4, 'Élévations latérales', 'epaules', 1),
+(5, 'Développé militaire', 'epaules,triceps', 1),
+(6, 'Curl biceps barre', 'biceps', 0),
+(7, 'Extension triceps à la poulie', 'triceps', 1),
+(8, 'Dips', 'pectoraux,triceps', 0),
+(9, 'Pull-over', 'pectoraux,dos', 1),
+(10, 'Face pull', 'dos,epaules', 1),
+(11, 'Pompes', 'pectoraux,triceps', 0),
+(12, 'Tirage horizontal', 'dos,biceps', 1),
+(13, 'Oiseau haltères', 'epaules', 0),
+(14, 'Curl incliné', 'biceps', 0),
+(15, 'Extension triceps barre', 'triceps', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seance`
+--
+
+CREATE TABLE `seance` (
+  `id` int NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seance_exercice`
+--
+
+CREATE TABLE `seance_exercice` (
+  `id` int NOT NULL,
+  `seance_id` int NOT NULL,
+  `exercice_id` int NOT NULL,
+  `ordre` int DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `exercice`
+--
+ALTER TABLE `exercice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seance`
+--
+ALTER TABLE `seance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seance_exercice`
+--
+ALTER TABLE `seance_exercice`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `seance_id` (`seance_id`),
+  ADD KEY `exercice_id` (`exercice_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `exercice`
+--
+ALTER TABLE `exercice`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `seance`
+--
+ALTER TABLE `seance`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `seance_exercice`
+--
+ALTER TABLE `seance_exercice`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `seance_exercice`
+--
+ALTER TABLE `seance_exercice`
+  ADD CONSTRAINT `seance_exercice_ibfk_1` FOREIGN KEY (`seance_id`) REFERENCES `seance` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `seance_exercice_ibfk_2` FOREIGN KEY (`exercice_id`) REFERENCES `exercice` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
